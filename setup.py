2edc247e040f7b2b548327ce7f94d4c4728ce201
Rosato, Matteo (contracted) <Matteo.Rosato@contracted.pmi.com>
2024-02-26 16:08:31 +0100

    First commit. Template created from cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for retrieving and analyzing property advertisements from real estate websites.',
+    author='Matteo Rosato',
+    license='AGPL-3.0 license',
+)
