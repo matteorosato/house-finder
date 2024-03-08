14887034ade3da3d7ca3876e90cfa3c296bb802a
matteorosato <matteorosato.dev@gmail.com>
2024-03-08 17:51:51 +0100

    Added Datasource and Idealista classes to handle the flow in an OOP way Defined several methods for getting results and process them Modified main function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,169 +1,196 @@
 # -*- coding: utf-8 -*-
 import base64
 import json
+import logging
 import os
 import pathlib
 import time
-
-import click
-import logging
-from pathlib import Path
-
 import pandas as pd
 import requests
 import toml
 from dotenv import find_dotenv, load_dotenv
 
+RAW = 'raw'  # name of the folder for raw data
+PROCESSED = 'processed'  # name of the folder for processed data
 
-def create_dataset(source_dir) -> pd.DataFrame:
-    json_files = [f for f in pathlib.Path(source_dir).glob("*.json")]
-    json_files.reverse()  # put files in descending order
-    dfs = []
-    for file in json_files:
-        with open(file, 'r') as f:
-            elements_dict = json.load(f)['elementList']
-            dfs.append(pd.DataFrame.from_dict(elements_dict))
-    merged_df = pd.concat(dfs)
-    merged_df = merged_df.drop_duplicates(subset=['propertyCode'], keep='first')
-    return merged_df
+# find .env automagically by walking up directories until it's found, then
+# load up the .env entries as environment variables
+load_dotenv(find_dotenv())
 
 
-def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
-    columns = ['propertyCode', 'floor', 'price', 'size', 'rooms', 'bathrooms', 'address', 'province', 'municipality',
-               'district', 'latitude', 'longitude', 'showAddress', 'url', 'distance', 'description', 'status',
-               'newDevelopment', 'hasLift', 'priceByArea', 'detailedType', 'hasPlan', 'hasStaging', 'topNewDevelopment',
-               'topPlus', 'externalReference', 'isAuction', 'parkingSpace', 'labels', 'highlight',
-               'newDevelopmentFinished']
-    df = df[columns]  # keep only specified columns
+class Datasource:
+    api_key = None
+    secret = None
 
-    # cast to int
-    df = df.astype({'price': 'int', 'size': 'int', 'priceByArea': 'int'})
+    logger = logging.getLogger(__name__)
 
-    # remove auction ads
-    df = df[df['isAuction'].isna()]
-    df = df.drop(columns=['isAuction'])
+    def __init__(self, name: str, config_filepath: str, data_dir: str):
+        self.name = name
+        self.config_filepath = config_filepath
+        self.data_dir = data_dir
+        self.df = None
 
-    # convert floors to numbers
-    df['floor'] = df['floor'].replace('ss', -1).replace('bj', 0).replace('en', 0.5)
+    @property
+    def filtered_params(self):
+        return self.parse_filter_params(
+            params_dict=self.read_toml_config(file_path=self.config_filepath))
 
-    return df
+    @property
+    def search_url(self):
+        return self.define_search_url()
 
+    @staticmethod
+    def read_toml_config(file_path: str) -> dict:
+        with open(file_path, 'r') as file:
+            config_dict = toml.load(file)
+        return config_dict
 
-def export_dataset(df, output_dir):
-    # export df for backup purposes
-    output_filename = os.path.join(output_dir, f'df_total_{int(time.time())}.csv')
-    df.to_csv(output_filename)
+    @staticmethod
+    def parse_filter_params(params_dict: dict) -> dict:
+        filtered_params = dict()
+        for dictionary in params_dict.values():
+            for k, v in dictionary.items():
+                if str(v):  # keep non-empty values only
+                    filtered_params[k] = v
+        return filtered_params
 
+    def create_dataset(self):
+        pass
 
-def get_oauth_token(api_key: str, secret: str) -> str:
-    message = f"{api_key}:{secret}"
+    def define_search_url(self) -> str:
+        pass
 
-    # deal with bytes-like object
-    message_bytes = message.encode('ascii')
-    base64_bytes = base64.b64encode(message_bytes)
-    base64_message = base64_bytes.decode('ascii')
+    def clean_dataset(self):
+        pass
 
-    auth_header = f"Basic {base64_message}"
+    def export_dataset(self):
+        # export df for backup purposes
+        output_filename = os.path.join(self.data_dir, PROCESSED, f'df_total_{int(time.time())}.csv')
+        self.df.to_csv(output_filename)
 
-    headers_dict = {"Authorization": auth_header,
-                    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"}
+    def get_oauth_token(self) -> str:
+        pass
 
-    params_dict = {"grant_type": "client_credentials",
-                   "scope": "read"}
+    def get_results(self) -> dict:
+        pass
 
-    try:
-        r = requests.post("https://api.idealista.com/oauth/token",
-                          headers=headers_dict,
-                          params=params_dict)
-        r.raise_for_status()
-        token = r.json()["access_token"]
-    except requests.exceptions.RequestException as e:
-        print(f"Connection error: '{str(e)}'")
-        raise
-
-    return token
-
-
-def read_toml_config(file_path: str) -> dict:
-    with open(file_path, 'r') as file:
-        config_dict = toml.load(file)
-    return config_dict
-
-
-def parse_filter_params(params_dict: dict) -> dict:
-    filtered_params = dict()
-    for dictionary in params_dict.values():
-        for k, v in dictionary.items():
-            if str(v):  # keep non-empty values only
-                filtered_params[k] = v
-    return filtered_params
+    def export_results(self, results: dict):
+        # export results from query
+        output_filename = os.path.join(self.data_dir, RAW, f'dump_{int(time.time())}.json')
+        self.logger.info(f'Exporting data to {output_filename}')
+        with open(output_filename, 'w') as f:
+            f.write(json.dumps(results, indent=4))
 
 
-def define_search_url(country: str) -> str:
-    search_url = f'https://api.idealista.com/3.5/{country}/search'
-    return search_url
+class Idealista(Datasource):
+    api_key: str = os.environ['IDEALISTA_API_KEY']
+    secret: str = os.environ['IDEALISTA_SECRET']
+
+    def __init__(self, name: str, config_filepath: str, data_dir: str):
+        super().__init__(name, config_filepath, data_dir)
+
+    def create_dataset(self):
+        source_dir = os.path.join(self.data_dir, RAW)
+        json_files = [f for f in pathlib.Path(source_dir).glob("*.json")]
+        json_files.reverse()  # put files in descending order
+        dfs = []
+        for file in json_files:
+            with open(file, 'r') as f:
+                elements_dict = json.load(f)['elementList']
+                dfs.append(pd.DataFrame.from_dict(elements_dict))
+        self.df = pd.concat(dfs)
+        # removing duplicates based on propertyCode
+        self.df.drop_duplicates(subset=['propertyCode'], keep='first')
+
+    def clean_dataset(self):
+        columns = ['propertyCode', 'floor', 'price', 'size', 'rooms', 'bathrooms', 'address', 'province',
+                   'municipality', 'district', 'latitude', 'longitude', 'showAddress', 'url', 'distance', 'description',
+                   'status', 'newDevelopment', 'hasLift', 'priceByArea', 'detailedType', 'hasPlan', 'hasStaging',
+                   'topNewDevelopment', 'topPlus', 'externalReference', 'isAuction', 'parkingSpace', 'labels',
+                   'highlight', 'newDevelopmentFinished']
+        self.df = self.df[columns]  # keep only specified columns
+
+        # cast to int
+        self.df = self.df.astype({'price': 'int', 'size': 'int', 'priceByArea': 'int'})
+
+        # remove auction ads
+        self.df = self.df[self.df['isAuction'].isna()]
+        self.df = self.df.drop(columns=['isAuction'])
+
+        # convert floors to numbers
+        self.df['floor'] = self.df['floor'].replace('ss', -1).replace('bj', 0).replace('en', 0.5)
+
+    def get_oauth_token(self) -> str:
+        message = f"{self.api_key}:{self.secret}"
+
+        # deal with bytes-like object
+        message_bytes = message.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('ascii')
+
+        auth_header = f"Basic {base64_message}"
+
+        headers_dict = {"Authorization": auth_header,
+                        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"}
+
+        params_dict = {"grant_type": "client_credentials",
+                       "scope": "read"}
+
+        try:
+            r = requests.post("https://api.idealista.com/oauth/token",
+                              headers=headers_dict,
+                              params=params_dict)
+            r.raise_for_status()
+            token = r.json()["access_token"]
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Connection error: '{str(e)}'")
+            raise
+
+        return token
+
+    def get_results(self) -> dict:
+        token = self.get_oauth_token()
+        headers_dict = {"Authorization": 'Bearer ' + token,
+                        "Content-Type": "application/x-www-form-urlencoded"
+                        }
+        try:
+            r = requests.post(self.search_url, headers=headers_dict, params=self.filtered_params)
+            r.raise_for_status()
+
+            result = r.json()
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Connection error: '{str(e)}'")
+            raise
+
+        return result
+
+    def define_search_url(self) -> str:
+        country = self.filtered_params['country']
+        search_url = f'https://api.idealista.com/3.5/{country}/search'
+        return search_url
 
 
-def get_results(url, params) -> dict:
-    token = get_oauth_token(IDEALISTA_API_KEY, IDEALISTA_SECRET)
-    headers_dict = {"Authorization": 'Bearer ' + token,
-                    "Content-Type": "application/x-www-form-urlencoded"
-                    }
-    try:
-        r = requests.post(url, headers=headers_dict, params=params)
-        r.raise_for_status()
-
-        result = r.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Connection error: '{str(e)}'")
-        raise
-
-    return result
-
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
 
-    config_filepath = 'config.toml'
-    params = read_toml_config(config_filepath)
-    filtered_params = parse_filter_params(params)
-    url = define_search_url(country=filtered_params['country'])
-    logger.info(f'Getting results from {url}')
-    result = get_results(url, filtered_params)
-
-    output_filename = f'data/raw/dump_{int(time.time())}.json'
-    logger.info(f'Exporting data to {output_filename}')
-    with open(output_filename, 'w') as f:
-        f.write(json.dumps(result, indent=4))
-
+    idealista = Idealista(name='Idealista', config_filepath='config.toml', data_dir='data')
+    logger.info(f'Getting results from {idealista.name} website')
+    results = idealista.get_results()
+    logger.info(f'Exporting results from {idealista.name} website')
+    idealista.export_results(results)
     logger.info('Creating dataset...')
-    df = create_dataset(input_filepath)
-
+    idealista.create_dataset()
     logger.info('Cleaning dataset...')
-    df = clean_dataset(df)
-
-    logger.info(f'Exporting dataset to {output_filepath}')
-    export_dataset(df, output_filepath)
+    idealista.clean_dataset()
+    logger.info(f'Exporting dataset...')
+    idealista.export_dataset()
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    IDEALISTA_API_KEY: str = os.environ['IDEALISTA_API_KEY']
-    IDEALISTA_SECRET: str = os.environ['IDEALISTA_SECRET']
+    logger = logging.getLogger(__name__)
 
     main()
