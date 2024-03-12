def82a349e1578d509e9798f7fe29c24283469ab
matteorosato <matteorosato.dev@gmail.com>
2024-03-12 12:50:02 +0100

    Added results pagination Redefined order of some methods

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,6 +12,7 @@ from dotenv import find_dotenv, load_dotenv
 
 RAW = 'raw'  # name of the folder for raw data
 PROCESSED = 'processed'  # name of the folder for processed data
+MAX_PAGES = 2  # limit of the ads pages to be requested
 
 # find .env automagically by walking up directories until it's found, then
 # load up the .env entries as environment variables
@@ -54,6 +55,19 @@ class Datasource:
                     filtered_params[k] = v
         return filtered_params
 
+    def get_oauth_token(self) -> str:
+        pass
+
+    def get_results(self) -> dict:
+        pass
+
+    def export_results(self, results: dict):
+        # export results from query
+        output_filename = os.path.join(self.data_dir, RAW, f'dump_{int(time.time())}.json')
+        self.logger.info(f'Exporting data to {output_filename}')
+        with open(output_filename, 'w') as f:
+            f.write(json.dumps(results, indent=4))
+
     def create_dataset(self):
         pass
 
@@ -68,19 +82,6 @@ class Datasource:
         output_filename = os.path.join(self.data_dir, PROCESSED, f'df_total_{int(time.time())}.csv')
         self.df.to_csv(output_filename)
 
-    def get_oauth_token(self) -> str:
-        pass
-
-    def get_results(self) -> dict:
-        pass
-
-    def export_results(self, results: dict):
-        # export results from query
-        output_filename = os.path.join(self.data_dir, RAW, f'dump_{int(time.time())}.json')
-        self.logger.info(f'Exporting data to {output_filename}')
-        with open(output_filename, 'w') as f:
-            f.write(json.dumps(results, indent=4))
-
 
 class Idealista(Datasource):
     api_key: str = os.environ['IDEALISTA_API_KEY']
@@ -89,36 +90,10 @@ class Idealista(Datasource):
     def __init__(self, name: str, config_filepath: str, data_dir: str):
         super().__init__(name, config_filepath, data_dir)
 
-    def create_dataset(self):
-        source_dir = os.path.join(self.data_dir, RAW)
-        json_files = [f for f in pathlib.Path(source_dir).glob("*.json")]
-        json_files.reverse()  # put files in descending order
-        dfs = []
-        for file in json_files:
-            with open(file, 'r') as f:
-                elements_dict = json.load(f)['elementList']
-                dfs.append(pd.DataFrame.from_dict(elements_dict))
-        self.df = pd.concat(dfs)
-        # removing duplicates based on propertyCode
-        self.df.drop_duplicates(subset=['propertyCode'], keep='first')
-
-    def clean_dataset(self):
-        columns = ['propertyCode', 'floor', 'price', 'size', 'rooms', 'bathrooms', 'address', 'province',
-                   'municipality', 'district', 'latitude', 'longitude', 'showAddress', 'url', 'distance', 'description',
-                   'status', 'newDevelopment', 'hasLift', 'priceByArea', 'detailedType', 'hasPlan', 'hasStaging',
-                   'topNewDevelopment', 'topPlus', 'externalReference', 'isAuction', 'parkingSpace', 'labels',
-                   'highlight', 'newDevelopmentFinished']
-        self.df = self.df[columns]  # keep only specified columns
-
-        # cast to int
-        self.df = self.df.astype({'price': 'int', 'size': 'int', 'priceByArea': 'int'})
-
-        # remove auction ads
-        self.df = self.df[self.df['isAuction'].isna()]
-        self.df = self.df.drop(columns=['isAuction'])
-
-        # convert floors to numbers
-        self.df['floor'] = self.df['floor'].replace('ss', -1).replace('bj', 0).replace('en', 0.5)
+    def define_search_url(self) -> str:
+        country = self.filtered_params['country']
+        search_url = f'https://api.idealista.com/3.5/{country}/search'
+        return search_url
 
     def get_oauth_token(self) -> str:
         message = f"{self.api_key}:{self.secret}"
@@ -154,20 +129,61 @@ class Idealista(Datasource):
                         "Content-Type": "application/x-www-form-urlencoded"
                         }
         try:
-            r = requests.post(self.search_url, headers=headers_dict, params=self.filtered_params)
-            r.raise_for_status()
+            elements = []
+            result = self.search(headers_dict)  # get results for the first page
+            self.logger.info(f"Available items: {result['total']} ({result['totalPages']} pages)")
+            elements.extend(result["elementList"])
 
-            result = r.json()
+            for i in range(2, min(MAX_PAGES, result["totalPages"]) + 1):
+                self.filtered_params["numPage"] = i
+                result = self.search(headers_dict)  # get results for the subsequent pages
+                elements.extend(result["elementList"])
+
+            result["elementList"] = elements  # update dictionary with cumulative results
+            self.logger.info(f"Stored {len(elements)} items over a total of {result['total']} available")
+
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Connection error: '{str(e)}'")
             raise
 
         return result
 
-    def define_search_url(self) -> str:
-        country = self.filtered_params['country']
-        search_url = f'https://api.idealista.com/3.5/{country}/search'
-        return search_url
+    def search(self, headers_dict) -> dict:
+        r = requests.post(self.search_url, headers=headers_dict, params=self.filtered_params)
+        r.raise_for_status()
+        result = r.json()
+        return result
+
+    def create_dataset(self):
+        source_dir = os.path.join(self.data_dir, RAW)
+        json_files = [f for f in pathlib.Path(source_dir).glob("*.json")]
+        json_files.reverse()  # put files in descending order
+        dfs = []
+        for file in json_files:
+            with open(file, 'r') as f:
+                elements_dict = json.load(f)['elementList']
+                dfs.append(pd.DataFrame.from_dict(elements_dict))
+        self.df = pd.concat(dfs)
+        # removing duplicates based on propertyCode
+        self.df.drop_duplicates(subset=['propertyCode'], keep='first')
+
+    def clean_dataset(self):
+        columns = ['propertyCode', 'floor', 'price', 'size', 'rooms', 'bathrooms', 'address', 'province',
+                   'municipality', 'district', 'latitude', 'longitude', 'showAddress', 'url', 'distance', 'description',
+                   'status', 'newDevelopment', 'hasLift', 'priceByArea', 'detailedType', 'hasPlan', 'hasStaging',
+                   'topNewDevelopment', 'topPlus', 'externalReference', 'isAuction', 'parkingSpace', 'labels',
+                   'highlight', 'newDevelopmentFinished']
+        self.df = self.df[columns]  # keep only specified columns
+
+        # cast to int
+        self.df = self.df.astype({'price': 'int', 'size': 'int', 'priceByArea': 'int'})
+
+        # remove auction ads
+        self.df = self.df[self.df['isAuction'].isna()]
+        self.df = self.df.drop(columns=['isAuction'])
+
+        # convert floors to numbers
+        self.df['floor'] = self.df['floor'].replace('ss', -1).replace('bj', 0).replace('en', 0.5)
 
 
 def main():
